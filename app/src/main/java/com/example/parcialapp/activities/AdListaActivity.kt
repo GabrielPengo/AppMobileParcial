e99c5a118979b0a7702c0320e9837e867f9a62a6
Guima <leolegal20032010@hotmail.com>
2024-10-05 13:05:45 -0300

    Imagem da addlist

--- app/src/main/java/com/example/parcialapp/activities/AdListaActivity.kt
+++ app/src/main/java/com/example/parcialapp/activities/AdListaActivity.kt
@@ -1,8 +1,12 @@
 package com.example.parcialapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.parcialapp.R
 import com.example.parcialapp.databinding.ActivityAdlistaBinding
 import com.example.parcialapp.db.ListasBD
 import com.example.parcialapp.entities.ListaDeCompras
@@ -18,6 +22,12 @@ class AdListaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val imagemSelecionada: ImageView = findViewById(R.id.floatingActionButton2)
+
+        binding.floatingActionButton2.setOnClickListener {
+            selectImage()
+        }
+
         usuario = intent.getSerializableExtra("usuarioLogado") as Usuario
 
         binding = ActivityAdlistaBinding.inflate(layoutInflater)
@@ -29,5 +39,11 @@ class AdListaActivity : AppCompatActivity() {
             listaBD.adLista(listaDeCompras)
             finish()
         }
+    }
+
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivity(intent)
     }
 }
